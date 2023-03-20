05e0f82452b0c8027d0b9f5880c08f2acd250b6a
pdr-dev71 <joaopedrosr2699@gmail.com>
2023-03-19 23:29:04 -0300

    added provider change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:crud_user_simple/provider/users_provider.dart';
+import 'package:crud_user_simple/routes/app_routes.dart';
+import 'package:crud_user_simple/view/user_form.dart';
 import 'package:crud_user_simple/view/user_list_view.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: UserListView(),
+    return ChangeNotifierProvider(
+      create: (context) => UsersProvider(),
+      child: MaterialApp(
+        routes: {
+          AppRoutes.HOME: (context) => const UserListView(),
+          AppRoutes.USER_FORM: (context) => const UserForm(),
+        },
+      ),
     );
   }
 }
