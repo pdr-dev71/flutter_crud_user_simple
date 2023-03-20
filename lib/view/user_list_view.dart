457c26e6ec458b14e88cd0b7a5d3f728a0fae1c2
pdr-dev71 <joaopedrosr2699@gmail.com>
2023-03-19 23:31:51 -0300

    changed of statisc fake users to ptovider

--- lib/view/user_list_view.dart
+++ lib/view/user_list_view.dart
@@ -1,21 +1,27 @@
-import 'package:crud_user_simple/data/fake_users_data.dart';
+import 'package:crud_user_simple/provider/users_provider.dart';
+import 'package:crud_user_simple/routes/app_routes.dart';
 import 'package:crud_user_simple/widgets/user_list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class UserListView extends StatelessWidget {
   const UserListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final users = {...FAKE_USERS};
+    final UsersProvider users = Provider.of(context);
     return Scaffold(
+      bottomNavigationBar: ElevatedButton(
+        child: const Text('Adicionar Usuário'),
+        onPressed: () => Navigator.of(context).pushNamed(AppRoutes.USER_FORM),
+      ),
       appBar: AppBar(
         title: const Text('Lista de Usuários'),
       ),
       body: ListView.builder(
-        itemCount: users.length,
+        itemCount: users.count,
         itemBuilder: (context, index) => UserListTile(
-          user: users.values.elementAt(index),
+          user: users.byIndex(index),
         ),
       ),
     );
